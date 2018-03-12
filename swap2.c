1bd20bef62a92ebed69fdbe4435b304db766c3d2
Mohammedsiddiq05 <31438748+Mohammedsiddiq05@users.noreply.github.com>
2018-03-12 15:51:22 +0530

    Create swap2.c

---
+++ swap2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("\nrnter the first number");
+scanf("%d",&a);
+printf("\nenter the second nummber");
+scanf("%d",&b);
+printf("\n****before swapping****");
+printf("\nthe values are :%d\n%d",a,b);
+printf("\n****after swapping***");
+printf("\nafter swapping the values are:%d\n%d",b,a);
+return 0;
+}
